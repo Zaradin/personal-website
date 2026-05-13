ec48075045cc254f9a56c2e9840d83cbfd0e9178
Josh Crotty <josh@MAC.station>
2026-05-13 23:09:18 +0100

    updated website layout and content

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,12 +4,13 @@ import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import remarkMath from "remark-math";
 import rehypeKatex from "rehype-katex";
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://astrofy-template.netlify.app",
   integrations: [mdx(), sitemap(), tailwind()],
-  integrations: [tailwind()],
+  integrations: [tailwind(), icon()],
   markdown: {
     remarkPlugins: [remarkMath],
     rehypePlugins: [rehypeKatex],
